d3838af375264f3df8c03230ab4827f9529301a3
Revenger7god <sakshamv339@gmail.com>
2025-05-25 20:35:52 +0530

    resetlink pop fix

--- Forgetpassword/resetlink.js
+++
@@ -1,8 +0,0 @@
-// reset-sent.js
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Auto-redirect back to login after 6 seconds
-  setTimeout(() => {
-    window.location.href = "/login area/form img.html"; // update path as needed
-  }, 6000);
-});
